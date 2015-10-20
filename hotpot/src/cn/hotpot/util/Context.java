ed51aeb98a7cbdb335739db3461c9c4d42bdb527
Administrator <Administrator@PC-201506051653>
2015-10-20 10:46:45 +0800

    Context文件补全

--- hotpot/src/cn/hotpot/util/Context.java
+++ hotpot/src/cn/hotpot/util/Context.java
@@ -2,32 +2,55 @@ package cn.hotpot.util;
 
 public class Context {
 	public static final String SEQ_NAME = "TY_SEQ";//oracle序列名
-	/**--订单状态可选数据**/
-	
-	
+	/**--订单状态可选数据  belong=1**/
 	public static final Integer ORDER_WAIT = 1;//等待确认
 	
 	public static final Integer ORDER_ACCEPT = 2;//订单已确认
 	
 	public static final Integer ORDER_FINISH = 3;//订单已完成
 	
-	public static final Integer MEMBER_TYPE_TIME = 4;//次卡
+	//员工状态belong=2
+	public static final Integer EMPLOYEE_ON_JOB = 4;//在职
+		
+	public static final Integer EMPLOYEE_OUT_JOB = 5;//离职
 	
-	public static final Integer MEMBER_TYPE_DAY = 5;//日卡
+	//员工工作中状态 belong=3
+	public static final Integer EMPLOYEE_JOB_FREE = 6;//空闲
+	
+	public static final Integer EMPLOYEE_JOB_BUSY = 7;//繁忙
+	
+	//餐桌状态 belong=4
+	public static final Integer TABLE_FREE = 8;//空闲
+	
+	public static final Integer TABLE_BUSY = 9;//占用
+	
+	//订单中需要的菜的状态 belong=5
+	public static final Integer DISH_WAIT = 10;//等待制作
+	
+	public static final Integer DISH_MAKING = 11;//制作中
+	
+	public static final Integer DISH_DELIVERY = 12;//配送中
+	
+	public static final Integer DISH_FINISH = 13;//制作完成
+	
+	//采购申请状态 belong=6
+	public static final Integer APPLY_WAIT = 14;//等待审核
+	
+	public static final Integer APPLY_PASS = 15;//审核通过
+	
+	public static final Integer APPLY_FAIL = 16;//审核失败
 	
 	/*--职位表数据-*/
 	public static final Integer POSITION_ADMIN = 1;//管理员
 	
-	public static final Integer POSITION_OPERATOR = 2;//操作员
+	public static final Integer POSITION_OPERATOR = 2;//营业员
 	
-	public static final Integer POSITION_COACH = 3;//普通教练
+	public static final Integer POSITION_HEAD_CHEF = 3;//厨师长
 	
-	public static final Integer POSITION_PRIVATE_COACH = 4;//私人教练
+	public static final Integer POSITION_WAITER = 4;//服务员
 	
-	/*------pay_method订单支付方式表里的数据------*/
-	public static final Integer PAY_METHOD_CASH = 1;//现金
+	public static final Integer POSITION_MANAGER = 5;//经理
 	
-	public static final Integer PAY_METHOD_BALANCE = 2;//卡余额
 	
 	/*------bill_type订单类型表里的数据------*/
 
@@ -44,38 +67,4 @@ public class Context {
 	public static final Integer BILL_TYPE_RETURN_BALANCE = 6;//退卡里余额
 	
 	
-	/*------status_type状态类型表里的数据------*/
-	public static final Integer STATUS_TYPE_EMPLOYEE = 1;//员工
-	
-	public static final Integer STATUS_TYPE_MEMBER = 2;//会员
-	
-	public static final Integer STATUS_TYPE_CABINET = 3;//衣柜
-	
-	public static final Integer STATUS_TYPE_CABINET_RECORD = 4;//衣柜出租记录
-	
-	/*------status表里的数据------*/
-	//员工状态
-	public static final Integer EMPLOYEE_ON_JOB = 1;//在职
-	
-	public static final Integer EMPLOYEE_OUT_JOB = 2;//离职
-	
-	//会员状态
-	public static final Integer MEMBER_VALID = 3;//可用
-	
-	public static final Integer MEMBER_LOCKED = 4;//禁用（锁定）
-	
-	public static final Integer MEMBER_UNVALID = 5;//注销（废弃）
-	
-	//衣柜状态
-	public static final Integer CABINET_VALID = 6;//可用
-	
-	public static final Integer CABINET_RENT = 7;//已租
-	
-	public static final Integer CABINET_ABANDON = 8;//废弃
-
-	
-	//衣柜记录状态
-	public static final Integer CABINET_RECORD_RENT = 9;//已租
-	
-	public static final Integer CABINET_RECORD_RETURN = 10;//返柜
 }
